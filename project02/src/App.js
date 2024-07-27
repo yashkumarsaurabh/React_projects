cf25fba6e2b0910a8b6e3ec6e144386e977f9f57
yashkumarsaurabh <65075425+yashkumarsaurabh@users.noreply.github.com>
2024-07-27 23:02:11 +0530

    added tailwind css & changed color according to useState

--- project02/src/App.js
+++ project02/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import "./App.css"
 function App() {
+  let [color, setColor] = useState("red");
   let [counter, setCounter] = useState(10);
 
   let addCounter = () => {
@@ -19,10 +19,34 @@ function App() {
   }
 
   return (
-    <div className="flex-box">
-      <div>Counter App: {counter}</div>
-      <div><button onClick={addCounter}>Add Counter</button>
-      <button onClick={removeCounter}>Remove Counter</button></div>
+    <div className="w-full h-screen duration-200" 
+    style={{backgroundColor: color}}>
+      <div className="pt-12 flex flex-wrap justify-center bottom-12 insert-x-0 px-2">
+        <div className="p-4">Counter : {counter}</div>
+        <div><button className="p-4 w-28 bg-black text-white rounded-3xl" onClick={addCounter}>Add</button>
+        <button className="w-28 ml-4 p-4 bg-black text-white rounded-3xl" onClick={removeCounter}>Remove</button></div>
+      </div>
+      <div className="pt-40 flex flex-wrap justify-center bottom-12 insert-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3
+        py-2 rounded-3xl">
+          <button
+          onClick={() => setColor("red")}
+          className="outline-none px-4 py-1 rounded-full"
+          style={{backgroundColor: "red"}}>Red</button>
+          <button
+          onClick={() => setColor("green")}
+          className="outline-none px-4 py-1 rounded-full"
+          style={{backgroundColor: "green"}}>Green</button>
+          <button
+          onClick={() => setColor("yellow")}
+          className="outline-none px-4 py-1 rounded-full"
+          style={{backgroundColor: "yellow"}}>Yellow</button>
+          <button
+          onClick={() => setColor("pink")}
+          className="outline-none px-4 py-1 rounded-full"
+          style={{backgroundColor: "pink"}}>Pink</button>
+        </div>
+      </div>
     </div>
   );
 }
